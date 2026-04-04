059b9ea6f6552f1b8cb028bca7fca306258cd272
oolong <yzhoupoipoi@gmail.com>
2026-04-03 21:57:01 -0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,11 @@ local opt = vim.opt
 opt.colorcolumn = "81"
 opt.termguicolors = true
 opt.relativenumber = false
+
+opt.wrap = true
+opt.linebreak = true
+opt.breakindent = true
+opt.showbreak = "↪ "
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
